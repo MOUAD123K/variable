53e6f13f3a9d0e72b6cc7c7cfc8cef601782f090
MOUAD <khshasht@gmail.com>
2024-09-26 18:44:43 +0200

    main

---
+++ Desktop/CONDITIONS/main.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+/*
+int main()
+{
+
+    float x ;
+    printf("veuillez entrer une nombre : ");
+    scanf("%f",&x);
+    if (x > 0 )
+        printf("positif");
+    else
+        if (x < 0)
+            printf("negatif");
+    else
+        printf("null");
+
+
+    return 0;
+
+
+}
+*/
+/*
+int main (){
+    float moyenne ;
+    printf("veuillez entrer le moyeen de note : ");
+    scanf("%f",&moyenne);
+
+    if(moyenne < 10 )
+        printf("recale");
+    else
+        if(moyenne > 10 && moyenne <= 12)
+         printf("passable") ;
+    else
+        if(moyenne > 12 && moyenne <= 14)
+           printf("assez bien");
+    else
+        if(moyenne > 14 &&  moyenne <= 16)
+        printf("bien");
+    else
+        if(moyenne > 16 && moyenne < 20)
+        printf("tree bien");
+    else
+        printf("le nombre incorecte ");
+
+
+
+
+
+
+
+
+
+
+
+
+ return 0 ;
+}
+*/
+/*
+int main ()
+{
+
+    int N ;
+    printf("veuillez enrer le nombre de joures : ");
+    scanf("%d",&N);
+    switch(N){
+        case 1:
+          printf("lundi");
+          break;
+        case 2 :
+          printf("mardi");
+          break;
+        case 3 :
+          printf("mercredi");
+          break;
+        case 4 :
+          printf("jeudi");
+          break;
+        case 5 :
+          printf("vendredi");
+          break;
+        case 6 :
+          printf("samedi");
+          break;
+        case 7 :
+          printf("dimanche");
+          break;
+        default : printf("le joure incorecte");
+        break;
+    }
+
+    return 0 ;
+
+}
+*/
+int main (){
+
+
+}
+
+
+
+
+
+
+
